c8e7ae060476556533d6a387886e47ad270f5283
OWoolland <oliver_woolland@hotmail.co.uk>
2023-03-13 21:15:39 +0000

    Remove non-working examples, rename working demo to texture.cu

--- texture.cu
+++ texture.cu
@@ -1,83 +1,119 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <iostream>
 
-__global__ void fetch(cudaTextureObject_t tex, std::size_t width, std::size_t height) 
+//typedef uint8_t float;  // use an integer type
+
+const int num_rows = 4;
+const int num_cols = 4;
+
+__global__ void kernel(cudaTextureObject_t tex, float xMax, float yMax)
 {
-  printf("\nWidth: %d, Height: %d\n", width, height);
+  float offset = 0.5;
+  float xScale = 1. / xMax;
+  float yScale = 1. / xMax;
 
-  for (int ii = 0 ; ii < height ; ++ii) {
-    for (int jj = 0 ; jj < width ; ++jj) {
-      float base = (jj*width+ii);
-      float u = base+0.5;
-      //float v = base+0.5;
-      auto a = tex1Dfetch<float>(tex, u);
-      //auto p = tex2Dfetch<float>(tex, u, v);
-      printf("u = %f\n", u);
-      //printf("v = %f\n", v);
-      printf("ax = %f\n", a);
-      //float ay = a.y;
-      //float az = a.z;
-      //printf("px %f, py %f, pz %f", px, py, pz);
-      //printf("u=%d v=%d\n\t ii %d width %d\n", u, v, ii, width);
+  printf("No offset \n");
+  for (int ii = 0; ii < num_rows; ++ii) {
+    for (int jj = 0; jj < num_cols; ++jj) {
+      float x = (float)jj;
+      float y = (float)ii;
+      float val = tex2D<float>(tex, (x+offset)*xScale, (y+offset)*yScale);
+      printf("%.2f, ", val);
     }
+    printf("\n");
   }
-
-  for (int j = 0; j < height; j++) {
-    //printf("j=%d\n",j);
-    for (int i = 0; i < width; i++) {
-
-
-      //auto p = tex2D<float>(tex, u, v);
-      //printf("i=%d, j=%d -> u=%f, v=%f, r=%d, g=%f, b=%f, a=%f\n", i, j, u, v, p.x, p.y, p.z, p.w);
-      //      printf("i=%d, j=%d\n",i,j);
-      //printf("i=%d, j=%d, px=%f, py=%f, pz=%f,");//,i,j,p.x,p.y,p.z);
-      // -> always returns p = {0, 0, 0, 0}
+  printf("\n");
+  
+  printf("X offset (half bin)\n");
+  for (int ii = 0; ii < num_rows; ++ii) {
+    for (int jj = 0; jj < num_cols; ++jj) {
+      float x = (float)jj+0.5;
+      float y = (float)ii;
+      float val = tex2D<float>(tex, (x+offset)*xScale, (y+offset)*yScale);
+      printf("%.2f, ", val);
     }
+    printf("\n");
   }
+  printf("\n");
+
+  printf("Y offset (half bin)\n");
+  for (int ii = 0; ii < num_rows; ++ii) {
+    for (int jj = 0; jj < num_cols; ++jj) {
+      float x = (float)jj;
+      float y = (float)ii+0.5;
+      float val = tex2D<float>(tex, (x+offset)*xScale, (y+offset)*yScale);
+      printf("%.2f, ", val);
+    }
+    printf("\n");
+  }
+  printf("\n");
+
+  printf("X and Y offset (half bin)\n");
+  for (int ii = 0; ii < num_rows; ++ii) {
+    for (int jj = 0; jj < num_cols; ++jj) {
+      float x = (float)jj+0.5;
+      float y = (float)ii+0.5;
+      float val = tex2D<float>(tex, (x+offset)*xScale, (y+offset)*yScale);
+      printf("%.2f, ", val);
+    }
+    printf("\n");
+  }
+  printf("\n");
 }
 
-int main() {
+//const int num_cols = prop.texturePitchAlignment*1; // should be able to use a different multiplier here
 
-  constexpr std::size_t width = 10;
-  constexpr std::size_t height = 10;
+int main(int argc, char **argv)
+{
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    printf("texturePitchAlignment: %lu\n", prop.texturePitchAlignment);
 
-  // creating a dummy texture
-  float image[width*height];
-  for(std::size_t jj = 0; jj < height; ++jj) {
-    for(std::size_t ii = 0; ii < width; ++ii) {
-      image[jj*width+ii] = (jj*width+ii);
-      std::cout << image[jj*width+ii] << " " ;
+    float xMax = num_cols;
+    float yMax = num_rows;
+    
+    cudaTextureObject_t tex;
+    float dataIn[num_cols*num_rows*sizeof(float)];
+    std::cout << "Input" << std::endl;
+    for (int ii = 0; ii < num_rows; ii++) {
+      for (int jj = 0; jj < num_cols; jj++) {
+        int index = (ii*num_cols)+jj;
+        dataIn[index] = (float)index*1.5;
+        std::cout << dataIn[index] << " ";
+      }
+      std::cout << std::endl;
     }
     std::cout << std::endl;
-  }
+    
+    float* dataDev = 0;
+    size_t pitch;
+    
+    cudaMallocPitch((void**)&dataDev, &pitch,  num_cols*sizeof(float), num_rows);
+    cudaMemcpy2D(dataDev, pitch, dataIn, num_cols*sizeof(float), num_cols*sizeof(float), num_rows, cudaMemcpyHostToDevice);
+    
+    struct cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    
+    resDesc.resType = cudaResourceTypePitch2D;
+    resDesc.res.pitch2D.devPtr = dataDev;
+    resDesc.res.pitch2D.width = num_cols;
+    resDesc.res.pitch2D.height = num_rows;
+    resDesc.res.pitch2D.desc = cudaCreateChannelDesc<float>();
+    resDesc.res.pitch2D.pitchInBytes = pitch;
+    
+    struct cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.addressMode[0]   = cudaAddressModeClamp;
+    texDesc.addressMode[1]   = cudaAddressModeClamp;
+    texDesc.filterMode       = cudaFilterModeLinear;
+    texDesc.readMode         = cudaReadModeElementType;
+    texDesc.normalizedCoords = true;
 
-  cudaArray_t cuArray;
-  auto channelDesc = cudaCreateChannelDesc<float>();
-  cudaMallocArray(&cuArray, &channelDesc, width, height);
-  cudaMemcpy2DToArray(cuArray, 0, 0, image, width*sizeof(float), width*sizeof(float), height, cudaMemcpyHostToDevice);
-
-  struct cudaResourceDesc resDesc;
-  memset(&resDesc, 0, sizeof(resDesc));
-  resDesc.resType = cudaResourceTypeArray;
-  resDesc.res.array.array = cuArray;
-
-  struct cudaTextureDesc texDesc;
-  memset(&texDesc, 0, sizeof(texDesc));
-  texDesc.addressMode[0]   = cudaAddressModeBorder;
-  texDesc.addressMode[1]   = cudaAddressModeBorder;
-  texDesc.filterMode       = cudaFilterModeLinear;
-  texDesc.readMode         = cudaReadModeElementType;
-  texDesc.normalizedCoords = 1;
-
-  cudaTextureObject_t texObj = 0;
-  cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
-
-  fetch<<<1, 1>>>(texObj, width, height);
-
-  cudaDeviceSynchronize();
-
-  cudaDestroyTextureObject(texObj);
-  cudaFreeArray(cuArray);
-
-  return 0;
+    cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL);
+    dim3 threads(1, 1);
+    kernel<<<1, threads>>>(tex, xMax, yMax);
+    cudaDeviceSynchronize();
+    printf("\n");
+    return 0;
 }
